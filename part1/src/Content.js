8b7b41a8c1bc5eddd24c5406a3a16c5dca484056
gregor murray <gregorian_rants@hotmail.com>
2022-08-22 14:40:25 +0100

    exercise 1.4

--- part1/src/Content.js
+++ part1/src/Content.js
@@ -1,8 +1,10 @@
-const Content = ({name,exercises})=>{
+const Content = ({parts})=>{
     return (
-        <p>
-            {name} {exercises}
-        </p>
+        <>
+            <p>{parts[0].name} {parts[0].exercises} </p>
+            <p>{parts[1].name} {parts[1].exercises}</p>
+            <p>{parts[2].name} {parts[2].exercises}</p>
+        </>
     )
 }
 
