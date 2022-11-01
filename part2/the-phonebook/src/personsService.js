327e080ace9e91e45c54270bb1d9a972e65cd14e
gregor murray <gregorian_rants@hotmail.com>
2022-11-01 09:25:42 +0000

    updated handling of api errors

--- part2/the-phonebook/src/personsService.js
+++ part2/the-phonebook/src/personsService.js
@@ -6,7 +6,8 @@ const baseUrl =
 const getAll = () => axios.get(baseUrl).then((res) => res.data);
 
 const create = (newPerson) =>
-  axios.post(baseUrl, newPerson).then((res) => res.data);
+  axios.post(baseUrl, newPerson)
+    .then((res) => res.data);
 
 const remove = (id) => axios.delete(`${baseUrl}/${id}`).then((res) => res);
 
